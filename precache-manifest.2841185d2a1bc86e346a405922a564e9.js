7ddd2e40766656332b7019e30086fd6e80938747
srikanthavadhanula <srikanth.a9500@gmail.com>
2020-08-12 14:32:49 +0530

    Updates

---
+++ precache-manifest.2841185d2a1bc86e346a405922a564e9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "81ffe06b084bfad7a1bcb010a9e0db61",
+    "url": "/divein/index.html"
+  },
+  {
+    "revision": "eb05097824125c7f69e1",
+    "url": "/divein/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "77fafda5909b7f0ded87",
+    "url": "/divein/static/js/2.3ae93777.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/divein/static/js/2.3ae93777.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "eb05097824125c7f69e1",
+    "url": "/divein/static/js/main.a1893ac2.chunk.js"
+  },
+  {
+    "revision": "8e0dc9b08c68b1bf4024",
+    "url": "/divein/static/js/runtime-main.d9f25eaa.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/divein/static/media/logo.5d5d9eef.svg"
+  }
+]);
